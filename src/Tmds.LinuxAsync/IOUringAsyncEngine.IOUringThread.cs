0fb0d4fdcab760892db3538002ce74e98cce0ac3
Adam Sitnik <adam.sitnik@gmail.com>
2020-03-02 17:32:30 +0100

    fix the condition to not throw on error (#46), fixes #44

--- src/Tmds.LinuxAsync/IOUringAsyncEngine.IOUringThread.cs
+++ src/Tmds.LinuxAsync/IOUringAsyncEngine.IOUringThread.cs
@@ -224,7 +224,7 @@ namespace Tmds.LinuxAsync
                 (AsyncOperationResult asyncResult, object? state, int data) =>
                     {
                         // TODO: do we need to do a volatile read of _disposed?
-                        if (asyncResult.Errno == EAGAIN || asyncResult.Value == 8)
+                        if (asyncResult.Errno == EAGAIN || (!asyncResult.IsError && asyncResult.Value == 8))
                         {
                             ((IOUringThread)state!).AddReadFromEventFd();
                         }
