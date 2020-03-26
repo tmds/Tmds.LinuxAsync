02e300d3904543a9d0cd54e6f6b59b275f2ecf16
Tom Deseyn <tom.deseyn@gmail.com>
2020-03-26 17:10:57 +0100

    Update doc

--- src/Tmds.LinuxAsync/AsyncOperation.cs
+++ src/Tmds.LinuxAsync/AsyncOperation.cs
@@ -54,26 +54,17 @@ namespace Tmds.LinuxAsync
         // Try to execute the operation. Returns true when done, false it should be tried again.
         public abstract bool TryExecuteSync();
 
+        // Asynchronously executes the operation on the io-thread.
+        // The AsyncExecutionQueue when provided may be used to batch operations.
+        // When the operation can make use of the executionQueue, AsyncExecutionResult.Executing is returned.
+        // When the operation cannot make use of the queue, the operation is attempted synchronously
+        // and WaitForPoll/Finished is returned.
         public abstract AsyncExecutionResult TryExecuteAsync(bool triggeredByPoll, AsyncExecutionQueue? executionQueue, AsyncExecutionCallback? callback, object? state, int data);
 
+        // Handles the result from the ExecutionQueue,
+        // and continues executing the operation it has not finished.
+        // Return value is the same as from TryExecuteAsync.
         public abstract AsyncExecutionResult HandleAsyncResultAndContinue(AsyncOperationResult result, AsyncExecutionQueue executionQueue, AsyncExecutionCallback? callback, object? state, int data);
-
-        // Continues execution of this operation.
-        // When the operation is finished, AsyncExecutionResult.Finished is returned.
-        // The executionQueue, when not null, can be used to batch operations.
-        //   The callback, state, and data arguments must be passed on to the executionQueue.
-        // When the executionQueue is used, AsyncExecutionResult.Executing is returned.
-        // When the batched operations completes, the method is called again and 'result' has a value.
-        // The execution queue may or may not support poll operations (ExecutionQueue.SupportsPolling).
-        // In case there is no execution queue, or the queue does not support polling, the method
-        // can return WaitForPoll. The method will be called again when poll indicates the handle is ready,
-        // (and triggeredByPoll is true).
-        // When asyncOnly is set, the execution queue must be used. If it cannot be used, WaitForPoll
-        // must be returned.
-        // When cancellationRequested is set, the operation must finish with
-        //   AsyncExecutionResult.Finished when the operation completed using 'result'; and
-        //   AsyncOperationResult.Cancelled otherwise.
-        // public abstract AsyncExecutionResult TryExecute(bool triggeredByPoll, bool cancellationRequested, bool asyncOnly, AsyncExecutionQueue? executionQueue, AsyncExecutionCallback? callback, object? state, int data, AsyncOperationResult result);
 
         // Requests operation to be cancelled.
         public void TryCancelAndComplete(OperationStatus status = OperationStatus.None)
