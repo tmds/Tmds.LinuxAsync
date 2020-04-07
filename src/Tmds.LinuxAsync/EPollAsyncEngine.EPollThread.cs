96b2f25b440d4afeff3d1bd5a65f5ab2424e531b
Anton Firszov <antonfir@gmail.com>
2020-04-08 01:34:45 +0200

    Use POH for dummyreadbuffers

--- src/Tmds.LinuxAsync/EPollAsyncEngine.EPollThread.cs
+++ src/Tmds.LinuxAsync/EPollAsyncEngine.EPollThread.cs
@@ -237,9 +237,10 @@ namespace Tmds.LinuxAsync
                 {
                     if (_dummyReadBuffer == null)
                     {
-                        _dummyReadBuffer = new byte[128];
+                        _dummyReadBuffer = GC.AllocateUninitializedArray<byte>(128, pinned: true);
                     }
-                    executionEngine.AddRead(_pipeReadEnd!, _dummyReadBuffer, this, data: 0);
+                    Memory<byte> memory = MemoryMarshal.CreateFromPinnedArray(_dummyReadBuffer, 0, _dummyReadBuffer.Length);
+                    executionEngine.AddRead(_pipeReadEnd!, memory, this, data: 0);
                 }
             }
 
