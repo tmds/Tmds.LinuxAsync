ff19c2aa5e84ba0893a55b009a4064beac136485
Tom Deseyn <tom.deseyn@gmail.com>
2020-04-20 17:40:41 +0200

    implement using EventCounter

--- src/Tmds.LinuxAsync/SocketsEventSource.cs
+++ src/Tmds.LinuxAsync/SocketsEventSource.cs
@@ -5,8 +5,15 @@ namespace Tmds.LinuxAsync
     [EventSource(Name = "SocketsEventSource")]
     internal class SocketsEventSource : EventSource
     {
-        [Event(1, Level = EventLevel.LogAlways)]
-        public void SocketEventsPerWait(int eventsCount) => WriteEvent(1, eventsCount);
+        private readonly EventCounter _socketEventsPerWait;
+
+        private SocketsEventSource()
+        {
+            _socketEventsPerWait = new EventCounter("EventsPerWait", this);
+        }
+
+        public void SocketEventsPerWait(int eventsCount)
+            => _socketEventsPerWait.WriteMetric(eventsCount);
 
         public static SocketsEventSource Log = new SocketsEventSource();
     }
